c7ba9e42593b103c11a6f978c0069c528f265d64
vadimkain <48841785+vadimkain@users.noreply.github.com>
2023-01-25 13:39:33 +0200

    Maven. 17. Default Lifecycle. Verify phase. Failsafe plugin

---
+++ src/test/java/com/kainv/integration/ExampleIT.java
@@ -0,0 +1,13 @@
+package com.kainv.integration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleIT {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
